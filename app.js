bca032eb64e9b9834377110e6d61001c178ec38c
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2025-05-12 21:00:20 +0700

    modul npm done

--- app.js
+++ app.js
@@ -15,3 +15,8 @@
 // const firstName = process.argv[2];
 // const lastName = process.argv[3];
 // console.log(`Hello ${firstName} ${lastName}`);
+
+// const coffee = require("./coffee");
+// console.log(coffee);
+
+const http = require("http");
--- index.js
+++ index.js
@@ -5,3 +5,7 @@
 
 // const memoryInformation = process.memoryUsage();
 // console.log(memoryInformation);
+
+const moment = require("moment");
+const date = moment().format("MMM Do YY");
+console.log(date);
