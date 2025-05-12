24a0bfd37f0289bb3959d4d74fc515313c36fd5e
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2025-05-12 20:30:58 +0700

    module 3.7

---
+++ index.js
@@ -0,0 +1,7 @@
+// const message = (name) => {
+//   console.log(`Hello ${name}!`);
+// };
+// message("John");
+
+// const memoryInformation = process.memoryUsage();
+// console.log(memoryInformation);
